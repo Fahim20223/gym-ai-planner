163337a063eb420f45574bd326f7e0289531af7c
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-05 18:44:51 +0600

    write the sign-in & sign-up with the user authentication

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,40 @@
 import { Dumbbell } from "lucide-react";
 import { Link } from "react-router-dom";
+import { Button } from "../ui/Button";
 
 export default function Navbar() {
+  const user = false;
   return (
     <header className="fixed top-0 left-0 right-0 z-50 border-b border-border bg-background/80 backdrop-blur-md">
       <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2">
-          <Dumbbell></Dumbbell>
-          <span>GymAI</span>
+        <Link to={"/"} className="flex items-center gap-2 text-foreground">
+          <Dumbbell className="w-6 h-6 text-accent"></Dumbbell>
+          <span className="font-semibold text-lg">GymAI</span>
         </Link>
+        <nav>
+          {user ? (
+            <>
+              {" "}
+              <Link to={"/auth/sign-in"}>
+                <Button variant="ghost" size="sm">
+                  My Plane
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to={"/auth/sign-in"}>
+                <Button variant="ghost" size="sm">
+                  Sing In
+                </Button>
+              </Link>
+              <Link to={"/auth/sign-up"}>
+                <Button size="sm">Sing Up</Button>
+              </Link>
+            </>
+          )}
+          <></>
+        </nav>
       </div>
     </header>
   );

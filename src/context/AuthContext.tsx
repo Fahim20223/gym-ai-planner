7fa75ce229a7f64d4d0b9ecd9e04edac1f7c752d
Fahim2023 <fahimshahrier2023@gmail.com>
2026-06-07 23:59:24 +0600

    add some more connection login in authcontext with neon for the user authenticated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import type { User } from "@neondatabase/neon-js/auth/types";
-import { createContext, useState, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import { authClient } from "../lib/auth";
 
 interface AuthUIContextType {
   user: User | null;
@@ -10,5 +11,25 @@ const AuthContext = createContext<AuthUIContextType | null>(null);
 export default function AuthProvider({ children }: { children: ReactNode }) {
   const [neoUser, setNeonUser] = useState<any>(null);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const result = await authClient.getSession();
+        if (result && result.data?.user) {
+          setNeonUser(result.data.user);
+        } else {
+          setNeonUser(null);
+        }
+      } catch (err) {
+        setNeonUser(null);
+      }
+    }
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: neoUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
